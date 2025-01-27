0808857a37ba14021dcc142a3f914f2d6d7aae54
greenwinther <dennisgw@live.se>
2025-01-27 13:49:56 +0100

    updated types

--- src/types/villager.ts
+++ src/types/villager.ts
@@ -51,3 +51,16 @@ export type Villager = {
   starSign: string;
   birthMonth: string;
 };
+
+// Type for sortable fields (buttons)
+export type SortableField =
+  | "name-asc"
+  | "name-desc"
+  | "species-asc"
+  | "species-desc"
+  | "personality-asc"
+  | "personality-desc"
+  | "gender-asc"
+  | "gender-desc"
+  | "birthday-asc"
+  | "birthday-desc";
