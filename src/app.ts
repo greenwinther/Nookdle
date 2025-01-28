98fe031d512f4bd0f0dcff3b6ca0116ce2a31fac
greenwinther <dennisgw@live.se>
2025-01-28 21:08:27 +0100

    Inputs and buttons now look at savedvillagers to

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,7 @@ export const initApp = async () => {
   const loading = createLoadingElement();
 
   let currentSort: SortableField = "name-asc";
+  let showingSaved = false;
 
   setupAppUI(
     appContainer,
@@ -29,27 +30,30 @@ export const initApp = async () => {
 
   const updateDisplay = (
     searchTerm = "",
-    sortBy: SortableField = currentSort
+    sortBy: SortableField = currentSort,
+    onlySaved: boolean = false
   ) => {
-    filterVillagers(searchTerm, sortBy);
+    filterVillagers(searchTerm, sortBy, onlySaved);
     updateSortButtons(sortBy);
   };
 
   try {
     await fetchAllVillagers();
     loading.remove();
-    updateDisplay();
+    updateDisplay("", currentSort);
   } catch (error) {
     console.error("Error:", error);
     loading.textContent = "Failed to load villagers";
   }
 
-  // A functions which puts a slight delay before displaying the content
+  // A function which handles search input updates with debounce
+  // If I type Ace real quick I dont want it to search on A then AC then ACE.
+  // This gives it a slight delay after I make an input
   const handleUpdate = () => {
     clearTimeout((handleUpdate as any).timeout);
     (handleUpdate as any).timeout = setTimeout(() => {
       const search = searchfield.searchInput.value;
-      updateDisplay(search);
+      updateDisplay(search, currentSort, showingSaved);
     }, 50);
   };
 
@@ -65,9 +69,19 @@ export const initApp = async () => {
       }
       currentSort = `${sortKey}-${currentDirection}` as SortableField;
       updateSortButtons(currentSort);
-      updateDisplay(searchfield.searchInput.value, currentSort);
+      updateDisplay(searchfield.searchInput.value, currentSort, showingSaved);
     }
   });
 
+  const showSavedVillagers = document.getElementById("showSavedVillagersBtn");
+  // Show only saved villagers
+  if (showSavedVillagers) {
+    showSavedVillagers.addEventListener("click", () => {
+      showingSaved = !showingSaved;
+      updateDisplay(searchfield.searchInput.value, currentSort, showingSaved);
+    });
+  }
+
+  // Handle search input event for searching villagers
   searchfield.searchInput.addEventListener("input", handleUpdate);
 };
