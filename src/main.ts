64208dc9abd31fab78d93b5983c04404d51b0132
greenwinther <dennisgw@live.se>
2025-01-28 19:01:36 +0100

    starts from a function. Failsafe if it doesnt work

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,6 @@
 import "./style.scss";
-/* import { startGame } from "./components/game";
-import { startGame2 } from "./components2/game2"; */
-/* import { startGame3 } from "./components3/game3"; */
-/* import { createGameUI } from "./might_use_later/componenets4/ui4"; */
-
-/* startGame(); */
-
-/* startGame2(); */
-
-/* startGame3(); */
-
-/* createGameUI(); */
-
 import { initApp } from "./app";
 
-document.addEventListener("DOMContentLoaded", initApp);
+/* // This ensures that the dom content has loaded before running the function
+document.addEventListener("DOMContentLoaded", initApp); */
+initApp();
