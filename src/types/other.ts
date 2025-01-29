1393785af5b87a4d0db9eea8ed6f338bb2798714
greenwinther <dennisgw@live.se>
2025-01-29 17:38:01 +0100

    moved type to new file

---
+++ src/types/other.ts
@@ -0,0 +1,12 @@
+// Type for sortable fields (buttons)
+export type SortableField =
+  | "name-asc"
+  | "name-desc"
+  | "species-asc"
+  | "species-desc"
+  | "personality-asc"
+  | "personality-desc"
+  | "gender-asc"
+  | "gender-desc"
+  | "birthday-asc"
+  | "birthday-desc";
