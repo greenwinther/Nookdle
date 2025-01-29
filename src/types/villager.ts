f9a823634e24753df2304ceccc704c4e86ddf65c
greenwinther <dennisgw@live.se>
2025-01-29 17:38:11 +0100

    moved type from file

--- src/types/villager.ts
+++ src/types/villager.ts
@@ -40,16 +40,3 @@ export type NookipediaCharacter = {
     umbrella: string;
   };
 };
-
-// Type for sortable fields (buttons)
-export type SortableField =
-  | "name-asc"
-  | "name-desc"
-  | "species-asc"
-  | "species-desc"
-  | "personality-asc"
-  | "personality-desc"
-  | "gender-asc"
-  | "gender-desc"
-  | "birthday-asc"
-  | "birthday-desc";
