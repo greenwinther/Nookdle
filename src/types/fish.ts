ff2cda9beaf6272c0be6985e8d2a7ba90960efb4
greenwinther <dennisgw@live.se>
2025-01-29 11:17:54 +0100

    created type for fishes

---
+++ src/types/fish.ts
@@ -0,0 +1,16 @@
+export type NookipediaFishes = {
+  url: string;
+  name: string;
+  number: number;
+  image_url: string;
+  render_url: string;
+  location: string;
+  shadow_size: string;
+  rarity: string;
+  total_catch: number;
+  sell_nook: number;
+  sell_cj: number;
+  tank_width: number;
+  tank_length: number;
+  catchphrases: string[];
+};
