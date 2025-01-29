3ea48f97b9d6e344b656992bf406d9a85c840d44
greenwinther <dennisgw@live.se>
2025-01-29 17:37:39 +0100

    added fetch to bug and fish

--- src/scripts/fetchVillagers.ts
+++ src/scripts/fetchVillagers.ts
@@ -1,7 +1,11 @@
 import { API_KEY } from "../apiKey";
 import type { NookipediaCharacter } from "../types/villager";
+import type { NookipediaBugs } from "../types/bugs";
+import type { NookipediaFish } from "../types/fish";
 
 export let allVillagers: NookipediaCharacter[] = [];
+export let allFish: NookipediaFish[] = [];
+export let allBugs: NookipediaBugs[] = [];
 
 // Fetches all villagers only if not already fetched
 export const fetchAllVillagers = async (): Promise<NookipediaCharacter[]> => {
@@ -15,4 +19,30 @@ export const fetchAllVillagers = async (): Promise<NookipediaCharacter[]> => {
     allVillagers = await response.json();
   }
   return allVillagers;
+};
+
+export const fetchAllFish = async (): Promise<NookipediaFish[]> => {
+  if (allFish.length === 0) {
+    const response = await fetch("https://api.nookipedia.com/nh/fish", {
+      headers: { "X-API-KEY": API_KEY },
+    });
+
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+    allFish = await response.json();
+  }
+  return allFish;
+};
+
+export const fetchAllBugs = async (): Promise<NookipediaBugs[]> => {
+  if (allBugs.length === 0) {
+    const response = await fetch("https://api.nookipedia.com/nh/bugs", {
+      headers: { "X-API-KEY": API_KEY },
+    });
+
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+    allBugs = await response.json();
+  }
+  return allBugs;
 };
